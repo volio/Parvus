28f5ca9af7fed7dbd28d0556d19c43facf9af2ea
volio <64764764+volio@users.noreply.github.com>
2023-10-13 19:51:45 -0700

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -27,38 +27,6 @@ local Window = Parvus.Utilities.UI:Window({
 }) do
 
     local CombatTab = Window:Tab({Name = "Combat"}) do
-        --[[local AimbotSection = CombatTab:Section({Name = "Aimbot",Side = "Left"}) do
-            AimbotSection:Toggle({Name = "Enabled",Flag = "Aimbot/Enabled",Value = false})
-            :Keybind({Flag = "Aimbot/Keybind",Value = "MouseButton2",Mouse = true,DisableToggle = true,
-            Callback = function(Key,KeyDown) Aimbot = Window.Flags["Aimbot/Enabled"] and KeyDown end})
-
-            AimbotSection:Toggle({Name = "Thirdperson Mode",Flag = "Aimbot/Thirdperson",Value = false})
-            AimbotSection:Toggle({Name = "Always Enabled",Flag = "Aimbot/AlwaysEnabled",Value = false})
-            AimbotSection:Toggle({Name = "Prediction",Flag = "Aimbot/Prediction",Value = false})
-
-            AimbotSection:Toggle({Name = "Team Check",Flag = "Aimbot/TeamCheck",Value = false})
-            AimbotSection:Toggle({Name = "Distance Check",Flag = "Aimbot/DistanceCheck",Value = false})
-            AimbotSection:Toggle({Name = "Visibility Check",Flag = "Aimbot/VisibilityCheck",Value = false})
-            AimbotSection:Slider({Name = "Sensitivity",Flag = "Aimbot/Sensitivity",Min = 0,Max = 100,Value = 20,Unit = "%"})
-            AimbotSection:Slider({Name = "Field Of View",Flag = "Aimbot/FieldOfView",Min = 0,Max = 500,Value = 100,Unit = "r"})
-            AimbotSection:Slider({Name = "Distance Limit",Flag = "Aimbot/DistanceLimit",Min = 25,Max = 1000,Value = 250,Unit = "studs"})
-
-            local PriorityList,BodyPartsList = {{Name = "Closest",Mode = "Button",Value = true}},{}
-            for Index,Value in pairs(KnownBodyParts) do
-                PriorityList[#PriorityList + 1] = {Name = Value[1],Mode = "Button",Value = false}
-                BodyPartsList[#BodyPartsList + 1] = {Name = Value[1],Mode = "Toggle",Value = Value[2]}
-            end
-
-            AimbotSection:Dropdown({Name = "Priority",Flag = "Aimbot/Priority",List = PriorityList})
-            AimbotSection:Dropdown({Name = "Body Parts",Flag = "Aimbot/BodyParts",List = BodyPartsList})
-        end
-        local AFOVSection = CombatTab:Section({Name = "Aimbot FOV Circle",Side = "Left"}) do
-            AFOVSection:Toggle({Name = "Enabled",Flag = "Aimbot/FOVCircle/Enabled",Value = true})
-            AFOVSection:Toggle({Name = "Filled",Flag = "Aimbot/FOVCircle/Filled",Value = false})
-            AFOVSection:Colorpicker({Name = "Color",Flag = "Aimbot/FOVCircle/Color",Value = {1,0.66666662693024,1,0.25,false}})
-            AFOVSection:Slider({Name = "NumSides",Flag = "Aimbot/FOVCircle/NumSides",Min = 3,Max = 100,Value = 14})
-            AFOVSection:Slider({Name = "Thickness",Flag = "Aimbot/FOVCircle/Thickness",Min = 1,Max = 10,Value = 2})
-        end]]
         local SilentAimSection = CombatTab:Section({Name = "Silent Aim",Side = "Left"}) do
             SilentAimSection:Dropdown({HideName = true,Flag = "SilentAim/Mode",List = {
                 {Name = "FindPartOnRayWithIgnoreList",Mode = "Toggle"},
@@ -105,39 +73,6 @@ local Window = Parvus.Utilities.UI:Window({
             SAFOVSection:Slider({Name = "NumSides",Flag = "SilentAim/FOVCircle/NumSides",Min = 3,Max = 100,Value = 14})
             SAFOVSection:Slider({Name = "Thickness",Flag = "SilentAim/FOVCircle/Thickness",Min = 1,Max = 10,Value = 2})
         end
-        --[[local TriggerSection = CombatTab:Section({Name = "Trigger",Side = "Right"}) do
-            TriggerSection:Toggle({Name = "Enabled",Flag = "Trigger/Enabled",Value = false})
-            :Keybind({Flag = "Trigger/Keybind",Value = "MouseButton2",Mouse = true,DisableToggle = true,
-            Callback = function(Key,KeyDown) Trigger = Window.Flags["Trigger/Enabled"] and KeyDown end})
-
-            TriggerSection:Toggle({Name = "Always Enabled",Flag = "Trigger/AlwaysEnabled",Value = false})
-            TriggerSection:Toggle({Name = "Hold Mouse Button",Flag = "Trigger/HoldMouseButton",Value = false})
-            TriggerSection:Toggle({Name = "Prediction",Flag = "Trigger/Prediction",Value = false})
-
-            TriggerSection:Toggle({Name = "Team Check",Flag = "Trigger/TeamCheck",Value = false})
-            TriggerSection:Toggle({Name = "Distance Check",Flag = "Trigger/DistanceCheck",Value = false})
-            TriggerSection:Toggle({Name = "Visibility Check",Flag = "Trigger/VisibilityCheck",Value = false})
-
-            TriggerSection:Slider({Name = "Click Delay",Flag = "Trigger/Delay",Min = 0,Max = 1,Precise = 2,Value = 0.15,Unit = "sec"})
-            TriggerSection:Slider({Name = "Distance Limit",Flag = "Trigger/DistanceLimit",Min = 25,Max = 1000,Value = 250,Unit = "studs"})
-            TriggerSection:Slider({Name = "Field Of View",Flag = "Trigger/FieldOfView",Min = 0,Max = 500,Value = 25,Unit = "r"})
-
-            local PriorityList,BodyPartsList = {{Name = "Closest",Mode = "Button",Value = true},{Name = "Random",Mode = "Button"}},{}
-            for Index,Value in pairs(KnownBodyParts) do
-                PriorityList[#PriorityList + 1] = {Name = Value[1],Mode = "Button",Value = false}
-                BodyPartsList[#BodyPartsList + 1] = {Name = Value[1],Mode = "Toggle",Value = Value[2]}
-            end
-
-            TriggerSection:Dropdown({Name = "Priority",Flag = "Trigger/Priority",List = PriorityList})
-            TriggerSection:Dropdown({Name = "Body Parts",Flag = "Trigger/BodyParts",List = BodyPartsList})
-        end
-        local TFOVSection = CombatTab:Section({Name = "Trigger FOV Circle",Side = "Right"}) do
-            TFOVSection:Toggle({Name = "Enabled",Flag = "Trigger/FOVCircle/Enabled",Value = true})
-            TFOVSection:Toggle({Name = "Filled",Flag = "Trigger/FOVCircle/Filled",Value = false})
-            TFOVSection:Colorpicker({Name = "Color",Flag = "Trigger/FOVCircle/Color",Value = {0.0833333358168602,0.6666666269302368,1,0.25,false}})
-            TFOVSection:Slider({Name = "NumSides",Flag = "Trigger/FOVCircle/NumSides",Min = 3,Max = 100,Value = 14})
-            TFOVSection:Slider({Name = "Thickness",Flag = "Trigger/FOVCircle/Thickness",Min = 1,Max = 10,Value = 2})
-        end]]
     end
     local VisualsSection = Parvus.Utilities:ESPSection(Window,"Visuals","ESP/Player",true,true,true,true,true,true) do
         VisualsSection:Colorpicker({Name = "Ally Color",Flag = "ESP/Player/Ally",Value = {0.3333333432674408,0.6666666269302368,1,0,false}})
@@ -154,8 +89,6 @@ Parvus.Utilities:SetupWatermark(Window)
 Parvus.Utilities:SetupLighting(Window.Flags)
 Parvus.Utilities.Drawing.SetupCursor(Window)
 Parvus.Utilities.Drawing.SetupCrosshair(Window.Flags)
---Parvus.Utilities.Drawing.FOVCircle("Aimbot",Window.Flags)
---Parvus.Utilities.Drawing.FOVCircle("Trigger",Window.Flags)
 Parvus.Utilities.Drawing.FOVCircle("SilentAim",Window.Flags)
 
 local WallCheckParams = RaycastParams.new()
@@ -182,6 +115,7 @@ end
 local function CalculateTrajectory(Origin,Velocity,Time,Gravity)
     return Origin + Velocity * Time + Gravity * Time * Time / GravityCorrection
 end
+
 local function GetClosest(Enabled,
     TeamCheck,VisibilityCheck,DistanceCheck,
     DistanceLimit,FieldOfView,Priority,BodyParts,
@@ -190,72 +124,71 @@ local function GetClosest(Enabled,
 
     if not Enabled then return end
     local CameraPosition,Closest = Camera.CFrame.Position,nil
-    for Index,Player in ipairs(PlayerService:GetPlayers()) do
-        if Player == LocalPlayer then continue end
+    local MissionsFolder = game.Workspace.Missions
+    if not MissionsFolder then return end
 
-        local Character = Player.Character if not Character then continue end
+    for _, mapModel in pairs(MissionsFolder:GetChildren()) do
+        if mapModel:IsA("Model") and mapModel:FindFirstChild("AISpawners") then
+            for _, spawner in pairs(mapModel.AISpawners:GetChildren()) do
+                local validSpawners = {"AISpawner", "AISpawnerSniper", "AISpawnerElite"}
+                for _, validSpawner in pairs(validSpawners) do
+                    if spawner.Name == validSpawner then
+                        local validEnemies = {"Bandit", "Shotgunner", "Elite"}
+                        for _, validEnemy in pairs(validEnemies) do
+                            local enemy = spawner:FindFirstChild(validEnemy)
+                            if enemy and enemy:IsA("Model") then
+                                local humanoid = enemy:FindFirstChild("Humanoid")
+                                if humanoid and humanoid.Health <= 0 then continue end
 
-        if not InEnemyTeam(TeamCheck,Player) then continue end
+                                for Index, BodyPart in ipairs(BodyParts) do
+                                    BodyPart = enemy:FindFirstChild(BodyPart)
+                                    if not BodyPart then continue end
 
-        local Humanoid = Character:FindFirstChildOfClass("Humanoid")
-        if not Humanoid then continue end if Humanoid.Health <= 0 then continue end
+                                    local BodyPartPosition = BodyPart.Position
+                                    local Distance = (BodyPartPosition - CameraPosition).Magnitude
+                                    if IsDistanceLimited(DistanceCheck,Distance,DistanceLimit) then continue end
+                                    if not IsVisible(VisibilityCheck,CameraPosition,BodyPartPosition,enemy) then continue end
 
-        for Index,BodyPart in ipairs(BodyParts) do
-            BodyPart = Character:FindFirstChild(BodyPart)
-            if not BodyPart then continue end
+                                    ProjectileGravity = Vector3.new(0,ProjectileGravity,0)
+                                    BodyPartPosition = PredictionEnabled and CalculateTrajectory(BodyPartPosition,
+                                    BodyPart.AssemblyLinearVelocity,Distance / ProjectileSpeed,ProjectileGravity) or BodyPartPosition
+                                    local ScreenPosition,OnScreen = Camera:WorldToViewportPoint(BodyPartPosition)
+                                    if not OnScreen then continue end
 
-            local BodyPartPosition = BodyPart.Position
-            local Distance = (BodyPartPosition - CameraPosition).Magnitude
-            if IsDistanceLimited(DistanceCheck,Distance,DistanceLimit) then continue end
-            if not IsVisible(VisibilityCheck,CameraPosition,BodyPartPosition,Character) then continue end
+                                    local Magnitude = (Vector2.new(ScreenPosition.X,ScreenPosition.Y) - UserInputService:GetMouseLocation()).Magnitude
+                                    if Magnitude >= FieldOfView then continue end
 
-            ProjectileGravity = Vector3.new(0,ProjectileGravity,0)
-            BodyPartPosition = PredictionEnabled and CalculateTrajectory(BodyPartPosition,
-            BodyPart.AssemblyLinearVelocity,Distance / ProjectileSpeed,ProjectileGravity) or BodyPartPosition
-            local ScreenPosition,OnScreen = Camera:WorldToViewportPoint(BodyPartPosition)
-            if not OnScreen then continue end
+                                    if Priority == "Random" then
+                                        Priority = KnownBodyParts[math.random(#KnownBodyParts)][1]
+                                        BodyPart = enemy:FindFirstChild(Priority)
+                                        if not BodyPart then continue end
 
-            local Magnitude = (Vector2.new(ScreenPosition.X,ScreenPosition.Y) - UserInputService:GetMouseLocation()).Magnitude
-            if Magnitude >= FieldOfView then continue end
+                                        BodyPartPosition = BodyPart.Position
+                                        BodyPartPosition = PredictionEnabled and CalculateTrajectory(BodyPartPosition,
+                                        BodyPart.AssemblyLinearVelocity,Distance / ProjectileSpeed,ProjectileGravity) or BodyPartPosition
+                                        ScreenPosition,OnScreen = Camera:WorldToViewportPoint(BodyPartPosition)
+                                    elseif Priority ~= "Closest" then
+                                        BodyPart = enemy:FindFirstChild(Priority)
+                                        if not BodyPart then continue end
 
-            if Priority == "Random" then
-                Priority = KnownBodyParts[math.random(#KnownBodyParts)][1]
-                BodyPart = Character:FindFirstChild(Priority)
-                if not BodyPart then continue end
+                                        BodyPartPosition = BodyPart.Position
+                                        BodyPartPosition = PredictionEnabled and CalculateTrajectory(BodyPartPosition,
+                                        BodyPart.AssemblyLinearVelocity,Distance / ProjectileSpeed,ProjectileGravity) or BodyPartPosition
+                                        ScreenPosition,OnScreen = Camera:WorldToViewportPoint(BodyPartPosition)
+                                    end
 
-                BodyPartPosition = BodyPart.Position
-                BodyPartPosition = PredictionEnabled and CalculateTrajectory(BodyPartPosition,
-                BodyPart.AssemblyLinearVelocity,Distance / ProjectileSpeed,ProjectileGravity) or BodyPartPosition
-                ScreenPosition,OnScreen = Camera:WorldToViewportPoint(BodyPartPosition)
-            elseif Priority ~= "Closest" then
-                BodyPart = Character:FindFirstChild(Priority)
-                if not BodyPart then continue end
-
-                BodyPartPosition = BodyPart.Position
-                BodyPartPosition = PredictionEnabled and CalculateTrajectory(BodyPartPosition,
-                BodyPart.AssemblyLinearVelocity,Distance / ProjectileSpeed,ProjectileGravity) or BodyPartPosition
-                ScreenPosition,OnScreen = Camera:WorldToViewportPoint(BodyPartPosition)
+                                    FieldOfView,Closest = Magnitude,{spawner,enemy,BodyPart,ScreenPosition}
+                                end
+                            end
+                        end
+                    end
+                end
             end
-
-            FieldOfView,Closest = Magnitude,{Player,Character,BodyPart,ScreenPosition}
         end
     end
 
     return Closest
 end
---[[local function AimAt(Hitbox,Sensitivity)
-    if not Hitbox then return end
-    if Window.Flags["Aimbot/Thirdperson"] then
-        mousemoverel(Hitbox[3].Position,true,Sensitivity)
-        return
-    end
-
-    local MouseLocation = UserInputService:GetMouseLocation()
-    mousemoverel(Vector2.new(
-        (Hitbox[4].X - MouseLocation.X) * Sensitivity,
-        (Hitbox[4].Y - MouseLocation.Y) * Sensitivity
-    ))
-end]]
 
 local OldIndex = nil
 OldIndex = hookmetamethod(game,"__index",function(Self,Index)
@@ -303,89 +236,4 @@ OldNamecall = hookmetamethod(game,"__namecall",function(Self,...)
     end
 
     return OldNamecall(Self,...)
-end)
-
---[[Parvus.Utilities.NewThreadLoop(0,function()
-    if not (Aimbot or Window.Flags["Aimbot/AlwaysEnabled"]) then return end
-
-    AimAt(GetClosest(
-        Window.Flags["Aimbot/Enabled"],
-        Window.Flags["Aimbot/TeamCheck"],
-        Window.Flags["Aimbot/VisibilityCheck"],
-        Window.Flags["Aimbot/DistanceCheck"],
-        Window.Flags["Aimbot/DistanceLimit"],
-        Window.Flags["Aimbot/FieldOfView"],
-        Window.Flags["Aimbot/Priority"][1],
-        Window.Flags["Aimbot/BodyParts"],
-        Window.Flags["Aimbot/Prediction"],
-        Window.Flags["Prediction/Velocity"],
-        Window.Flags["Prediction/Gravity"]
-    ),Window.Flags["Aimbot/Sensitivity"] / 100)
-end)]]
-Parvus.Utilities.NewThreadLoop(0,function()
-    SilentAim = GetClosest(
-        Window.Flags["SilentAim/Enabled"],
-        Window.Flags["SilentAim/TeamCheck"],
-        Window.Flags["SilentAim/VisibilityCheck"],
-        Window.Flags["SilentAim/DistanceCheck"],
-        Window.Flags["SilentAim/DistanceLimit"],
-        Window.Flags["SilentAim/FieldOfView"],
-        Window.Flags["SilentAim/Priority"][1],
-        Window.Flags["SilentAim/BodyParts"],
-        Window.Flags["SilentAim/Prediction"],
-        Window.Flags["Prediction/Velocity"],
-        Window.Flags["Prediction/Gravity"]
-    )
-end)
---[[Parvus.Utilities.NewThreadLoop(0,function()
-    if not (Trigger or Window.Flags["Trigger/AlwaysEnabled"]) then return end
-    --if not iswindowactive() then return end
-
-    local TriggerClosest = GetClosest(
-        Window.Flags["Trigger/Enabled"],
-        Window.Flags["Trigger/TeamCheck"],
-        Window.Flags["Trigger/VisibilityCheck"],
-        Window.Flags["Trigger/DistanceCheck"],
-        Window.Flags["Trigger/DistanceLimit"],
-        Window.Flags["Trigger/FieldOfView"],
-        Window.Flags["Trigger/Priority"][1],
-        Window.Flags["Trigger/BodyParts"],
-        Window.Flags["Trigger/Prediction"],
-        Window.Flags["Prediction/Velocity"],
-        Window.Flags["Prediction/Gravity"]
-    ) if not TriggerClosest then return end
-
-    task.wait(Window.Flags["Trigger/Delay"]) mouse1press()
-    if Window.Flags["Trigger/HoldMouseButton"] then
-        while task.wait() do
-            TriggerClosest = GetClosest(
-                Window.Flags["Trigger/Enabled"],
-                Window.Flags["Trigger/TeamCheck"],
-                Window.Flags["Trigger/VisibilityCheck"],
-                Window.Flags["Trigger/DistanceCheck"],
-                Window.Flags["Trigger/DistanceLimit"],
-                Window.Flags["Trigger/FieldOfView"],
-                Window.Flags["Trigger/Priority"][1],
-                Window.Flags["Trigger/BodyParts"],
-                Window.Flags["Trigger/Prediction"],
-                Window.Flags["Prediction/Velocity"],
-                Window.Flags["Prediction/Gravity"]
-            ) if not TriggerClosest or not Trigger then break end
-        end
-    end mouse1release()
-end)]]
-
-Workspace:GetPropertyChangedSignal("CurrentCamera"):Connect(function()
-    Camera = Workspace.CurrentCamera
-end)
-
-for Index,Player in pairs(PlayerService:GetPlayers()) do
-    if Player == LocalPlayer then continue end
-    Parvus.Utilities.Drawing:AddESP(Player,"Player","ESP/Player",Window.Flags)
-end
-PlayerService.PlayerAdded:Connect(function(Player)
-    Parvus.Utilities.Drawing:AddESP(Player,"Player","ESP/Player",Window.Flags)
-end)
-PlayerService.PlayerRemoving:Connect(function(Player)
-    Parvus.Utilities.Drawing:RemoveESP(Player)
 end)
